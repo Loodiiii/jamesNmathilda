8eee11d66e578c411e10ad8425785655b4307494
PetarNovitovic <pnovitovic1@gmail.com>
2020-11-11 01:17:07 +0100

    slider has been finished, along with other sections, everything besides responsiveness and our services section

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,3 +1,18 @@
+// slider hero
+
+document.addEventListener( 'DOMContentLoaded', function () {
+    new Splide( '.splide', {
+        padding: {
+            left : 0,
+            right: 0,
+        },
+        perPage: 1,
+        arrows: false,
+    } ).mount();
+} );
+
+// getting year for footer
+
 const currentYear = new Date().getFullYear()
 const yearHTML = document.querySelector("#currentYear")
 

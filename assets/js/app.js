9d5b5c72585b29dbd31062e55bdf315b592f99f2
PetarNovitovic <pnovitovic1@gmail.com>
2020-11-15 23:41:29 +0100

    project has been finished, only final touches remain

--- assets/js/app.js
+++ assets/js/app.js
@@ -35,17 +35,31 @@ window.addEventListener("click", function() {
 
 const menuBtn = document.querySelector('.menu-btn');
 let menuOpen = false;
+const nav = document.querySelector("nav")
+
 menuBtn.addEventListener('click', () => {
+    navOpens();
+});
+
+nav.addEventListener("click", () => {
+    navOpens();
+})
+
+const menuBtnBurger = document.querySelector('.menu-btn__burger');
+const orderedServices = document.querySelector(".ordered-services")
+function navOpens(){
     if(!menuOpen) {
         menuBtn.classList.add('open');
         menuOpen = true;
+        nav.classList.add("nav-open")
+        orderedServices.classList.add("ordered-services-open")
     } else {
         menuBtn.classList.remove('open');
         menuOpen = false;
+        nav.classList.remove("nav-open")
+        orderedServices.classList.remove("ordered-services-open")
     }
-});
-
-const menuBtnBurger = document.querySelector('.menu-btn__burger');
+}
 
 // slider hero
 
@@ -67,7 +81,7 @@ const buttons = document.querySelectorAll(".hovering-buttons")
 const arrows = document.querySelectorAll(".hovering-arrow")
 const anchors = document.querySelectorAll(".hovering-anchor")
 
-for(let i = 0;i<buttons.length;i++){
+for(let i = 0; i < buttons.length; i++){
     buttons[i].addEventListener("mouseover", () => {
         buttons[i].style.backgroundColor = "rgb(255, 228, 114)"
         anchors[i].style.color = "rgb(47, 58, 62)"
@@ -104,25 +118,25 @@ window.addEventListener("scroll", () => {
 
     if(windowYoffset < ourServicesTop){
         let p = 0;
-        while(p <= listElements.length){
+        while(p < listElements.length){
             if(listElements[p].classList.contains("nav-current-section")){
                 listElements[p].classList.remove("nav-current-section")
             }
             p++;
         }
         i = 0;
-    } else if (ourServicesTop < windowYoffset && windowYoffset < contactUsTop){
+    } else if (ourServicesTop - 1 < windowYoffset && windowYoffset < contactUsTop){
         let p = 0;
-        while(p <= listElements.length){
+        while(p < listElements.length){
             if(listElements[p].classList.contains("nav-current-section")){
                 listElements[p].classList.remove("nav-current-section")
             }
             p++;
         }
         i = 1;
-    } else if(contactUsTop < windowYoffset && windowYoffset < aboutUsTop){
+    } else if(contactUsTop - 1 < windowYoffset && windowYoffset < aboutUsTop){
         let p = 0;
-        while(p <= listElements.length){
+        while(p < listElements.length){
             if(listElements[p].classList.contains("nav-current-section")){
                 listElements[p].classList.remove("nav-current-section")
             }
@@ -131,7 +145,7 @@ window.addEventListener("scroll", () => {
         i = 2;
     } else{
         let p = 0;
-        while(p <= listElements.length){
+        while(p < listElements.length){
             if(listElements[p].classList.contains("nav-current-section")){
                 listElements[p].classList.remove("nav-current-section")
             }
@@ -143,30 +157,22 @@ window.addEventListener("scroll", () => {
     listElements[i].classList.add("nav-current-section")
 })
 
-
-
 // ordering
 
 const items = document.querySelectorAll(".order-item")
-function ordering(){
-    for(let i = 0; i <= items.length; i++){
-        items[i].addEventListener("click", function(){
-            if(items[i].classList.contains("green")){
-                items[i].classList.remove("green")
-            }else{
-                items[i].classList.add("green")
-            }
-            
-        const orderedItems = document.querySelectorAll(".green")
-        let orderedGreenItems = document.querySelector("#ordered-items")
-        orderedGreenItems.innerHTML = "(" + orderedItems.length + ")";
-        })
-    }
+
+for(let i = 0; i < items.length; i++){
+    items[i].addEventListener("click", function(){
+        if(items[i].classList.contains("green")){
+            items[i].classList.remove("green")
+        }else{
+            items[i].classList.add("green")
+        }
+        
+    const orderedItems = document.querySelectorAll(".green")
+    let orderedGreenItems = document.querySelector("#ordered-items")
+    orderedGreenItems.innerHTML = "(" + orderedItems.length + ")";
+    })
 }
-
-window.addEventListener("click", () => {
-    ordering();
-})
-
 
 
